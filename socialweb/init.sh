6626a38ce2530fe384f82cee37986842090767c8
Uroš Malešević <juros1234@gmail.com>
2024-07-30 16:06:54 +0200

    Feature/tls and autoscale (#1)
    * Add the postgres db and the cert manager as helm dependencies
    
    * Add a self signed cert issuer
    
    * Add a metrics server to the chart
    
    * Add horizontal pod autoscaler
    
    * Remove values from pet store

--- socialweb/init.sh
+++ socialweb/init.sh
@@ -1,6 +1,11 @@
 echo "Creating kind cluster"
 kind create cluster --config ./kind.yaml
 
+helm repo add jetstack https://charts.jetstack.io  
+helm repo add bitnami https://charts.bitnami.com/bitnami 
+helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
 echo "Installing ingress"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
@@ -8,11 +13,10 @@ sleep 20s
 
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
 
+helm install prometheus prometheus-community/prometheus 
+
 echo "Creating namespace"
 kubectl create ns social-web-dev
-
-echo "Installing postgres database"
-helm install postgres-database oci://registry-1.docker.io/bitnamicharts/postgresql -n social-web-dev  --set global.postgresql.auth.postgresPassword=password,global.postgresql.auth.database=socialweb
 
 echo "Running helm chart"
 helm install social-web . -n social-web-dev
